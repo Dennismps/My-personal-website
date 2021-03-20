32b12b8592fe04f0ddab8c768826755581928f80
Dennismps <79340987+Dennismps@users.noreply.github.com>
2021-03-20 03:28:33 -0600

    major changes

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
-var cookies = 0;
+var cookiecount = 0;
 var cpc = 1;
 var bakerprice = 20;
+var autoclicker = 0;
+
+function update(){
+    document.getElementById('text').innerHTML = "Cookies: " + cookiecount;
+    document.title = cookiecount + "Cookies";
+    document.getElementById('amountAutoClick').innerHTML = "You Own " + autoclicker + " Auto Clickers";
+    document.getElementById('costAutoCliker').innerHTML = ((autoclicker+1) * 12) + "Cookies";
+}
+
+
+
+function timer() {
+    cookiecount = cookiecount + autoclicker;
+    update()
+}
+setInterval(timer, 1000)
+
+function buyAutoClicker() {
+    if (cookiecount >= ((autoclicker+1) * 12)) {
+        cookiecount = cookiecount - ((autoclicker+1) * 12);
+        autoclicker = autoclicker +1;
+        update()
+        
+    }
+}
+
+
+function save() {
+    localStorage.setItem("cookiecount", cookiecount);
+    localStorage.setItem("autoClick", autoclicker);
+}
+
+function load() {
+    cookiecount = localStorage.getItem("cookiecount");
+    cookiecount = parseInt(cookiecount);
+    autoclicker = localStorage.getItem("autoClick");
+    autoclicker = parseInt(autoclicker);
+    update()
+}
+
+
+
 function bakeCookie() {
-    cookies += cpc;
-    document.getElementById('cookie-text').innerHTML = "Cookies: " + cookies;
+    cookiecount += cpc;
+    update()
 
 }
 
 function hireBaker() {
-    if(cookies >= bakerprice) {
+    if(cookiecount >= bakerprice) {
         cpc += 1;
-        cookies -=bakerprice;
+        cookiecount -=bakerprice;
         bakerprice = Math.round(bakerprice * 1.7) ;
-        document.getElementById('cookie-text').innerHTML = "Cookies: " + cookies;
+        document.getElementById('text').innerHTML = "Cookies: " + cookiecount;
         document.getElementById('bakerPriceText').innerHTML = "Bakerprice: " + bakerprice;
     } else {
         alert("You dont have enough cookies")
